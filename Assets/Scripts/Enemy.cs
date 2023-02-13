265946e5a5cf955a5eda2cbe26a1391a4d9f787d
Dmitriy Tuchashvili <pechorin211@gmail.com>
2023-02-13 17:59:17 +0600

    initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Entity
+{
+    [SerializeField] private float _moveSpeed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 moveVector = Vector3.up * _moveSpeed * Time.deltaTime;
+        transform.Translate(moveVector, Space.Self);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.attachedRigidbody.TryGetComponent(out Player player))
+        {
+            player.TakeDamage(1);
+            Destroy(gameObject);
+        }
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        Destroy(gameObject);
+    }
+}
